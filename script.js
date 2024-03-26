a1c3eede937121ea443a8bf59ea34f5e8a329288
Leandro Henrique <leandrohenriquetti@gmail.com>
2024-03-26 16:25:02 -0300

    Resistência à erros com catch

--- script.js
+++ script.js
@@ -9,28 +9,33 @@ btn.addEventListener("click", function() {
         hidden.innerHTML = "Se você não digitar nada me complica né paizão"
         hidden.className = "not-hidden"
     }
-    if(input.value > 389 || input.value < 1){
+    /*if(input.value > 389 || input.value < 1){
         hidden.innerHTML = "Digite um número entre 1 e 389 amigão"
         hidden.className = "not-hidden"
-    }
+    }*/
     else{
         hidden.className = "hidden"
         fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${input.value}`)
-        .then((response => response.json()))
+        .then((response => {
+            if(response.status == 200){
+                return response.json()
+            }
+            else{
+                throw new Error("Deu ruim")
+            }
+        }))
         .then((datas => {
             console.log(datas.data)
             const result = datas.data
             const newLI = document.createElement('li');
             if(result !== undefined){
                 if(result.common_locations){
-                    for(i = 0; i < result.common_locations.length; i++){
-                        newLI.innerHTML = `Lugar comuns para se encontrar: ${result.common_locations} <br>` ;
-                    }
+                    newLI.innerHTML = `Lugar comuns para se encontrar: ${result.common_locations} <br>` ;                    
                 }
                 if(result.description){
                     newLI.innerHTML = newLI.innerHTML + `Descrição: ${result.description} <br>` ;
                 }
-                if(result.drops.length > 0){
+                if(result.drops !== undefined &&  result.drops !== null && result.drops.length > 0){
                     newLI.innerHTML = newLI.innerHTML + ` Drops: ${result.drops} <br>` ;
                 }
                 if(result.name !== undefined){
@@ -45,5 +50,9 @@ btn.addEventListener("click", function() {
                 }
             }
         }))
+        .catch((error) => {
+            hidden.innerHTML = "Digite um número entre 1 e 389 ou um nome de uma entidade existente"
+            hidden.className = "not-hidden"
+        })
     } 
 })
